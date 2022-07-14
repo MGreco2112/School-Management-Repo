2d58281913f65257c208ad0004292f9002d4fb5e
Matthew Greco <mgreco2112@tutanota.com>
2022-07-14 19:36:54 -0400

    initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        School:
+            Teachers,
+            Students,
+            Money in,
+            Money out
+
+        Teachers:
+            id,
+            name,
+            salary
+
+        Students:
+            id,
+            name,
+            grade,
+            fees paid,
+            fees total
+         */
+
+	// write your code here
+    }
+}
